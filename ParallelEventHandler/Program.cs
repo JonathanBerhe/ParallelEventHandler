ad4cc1154cfa3627d4a1f27468f6534085a962e7
Jonathan Berhe <jonathan.berhe.work@gmail.com>
2018-10-27 16:07:32 +0200

    Aggiungere i file di progetto.

---
+++ ParallelEventHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParallelEventHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
